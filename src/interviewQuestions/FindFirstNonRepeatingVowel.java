f8f9bb7c177500969de074c2f42a6dd7ba546dce
BAHATTIN TASTAN <59250233+bahatas@users.noreply.github.com>
2022-06-09 01:04:51 +0100

    I added a question for interviews

---
+++ src/interviewQuestions/FindFirstNonRepeatingVowel.java
@@ -0,0 +1,47 @@
+package interviewQuestions;
+
+public class FindFirstNonRepeatingVowel {
+
+    public static void main(String[] args) {
+        String name = "adelina"; // 2
+
+        FindFirstNonRepeatingVowel obj = new FindFirstNonRepeatingVowel();
+
+        System.out.println("obj.findfirstvowel(name) = " + obj.findfirstvowel(name));
+    }
+    public  int findfirstvowel(String word) {
+
+//adelina //2
+//        kadifa  a2  i1  --> 3
+//       Check each chars one by one if it is vowel
+        for (int i = 0; i < word.length(); i++) {
+
+            char currentChar = word.charAt(i);
+            if (currentChar == 'a' || currentChar == 'e' || currentChar == 'i' || currentChar == 'u' || currentChar == 'o') {
+
+                // check others from begining if there is same
+                int counter = 0;
+
+                for (int j = 0; j < word.length(); j++) {
+                    //       If there is same vowel count them
+                    if (currentChar == word.charAt(j)) {
+                        counter++;
+                    }
+                }
+//                Count==1 return its index
+
+                if (counter == 1) {
+                    return i;
+                }
+            }
+        }
+
+
+//
+
+
+        return -1;
+    }
+
+
+}
